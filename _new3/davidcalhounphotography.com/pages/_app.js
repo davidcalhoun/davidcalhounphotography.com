8eb74e3e06e271818b758300074c1bc9930b207b
David Calhoun <david.b.calhoun@gmail.com>
2021-11-22 08:25:40 -0500

    fix google analytics

--- _new3/davidcalhounphotography.com/pages/_app.js
+++ _new3/davidcalhounphotography.com/pages/_app.js
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
+import '../styles/globals.css';
 
 function CustomApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize('UA-83942647-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [pageProps])
+
   return <Component {...pageProps} />
 }
 
